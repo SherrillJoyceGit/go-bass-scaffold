a4b939d5145869e3cfdced3841afa5322f7660c4
SherrillJoyceGit <43162021+SherrillJoyceGit@users.noreply.github.com>
2022-07-25 14:47:17 +0800

    数据库部分修改完成

--- db/config.go
+++ db/config.go
@@ -1,5 +1,7 @@
 package db
 
+import "errors"
+
 // Config defines the config of db something
 type Config struct {
 	DbType   string
@@ -11,3 +13,18 @@ type Config struct {
 }
 
 var CurrentConfig = Config{}
+
+func getCurrentConfig() (*Config, error) {
+	if CurrentConfig.DbType == "" ||
+		CurrentConfig.DbName == "" ||
+		CurrentConfig.Username == "" ||
+		CurrentConfig.Password == "" ||
+		CurrentConfig.Host == "" ||
+		CurrentConfig.Port == "" {
+
+		return nil, errors.New("current config is not be settled ! ")
+
+	} else {
+		return &CurrentConfig, nil
+	}
+}
--- db/db.go
+++ db/db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"fmt"
-	"github.com/SherrillJoyceGit/go-bass-scaffold/config"
 	"github.com/SherrillJoyceGit/go-bass-scaffold/middle/logger"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -10,50 +9,52 @@ import (
 	"log"
 )
 
-var Cloud *gorm.DB
+var db *gorm.DB
 
-func init() {
+func NewDbAccess() (*gorm.DB, error) {
 	var (
 		err                                        error
 		dbType, dbName, user, password, host, port string
 	)
 
 	//section, err := setting.Cfg.GetSection("database")
-
-	if err != nil {
+	cfg, err := getCurrentConfig()
+	if err == nil {
 		//log.Fatal(2, "Fail to get section 'database': %v", err)
 		logger.LoggerCurrent().Fatal("Fail to get section 'database': " + err.Error())
 	}
 
-	dbType = config.DbConfig.DbType
-	dbName = config.DbConfig.DbName
-	user = config.DbConfig.User
-	password = config.DbConfig.Password
-	host = config.DbConfig.Host
-	port = config.DbConfig.Port
+	dbType = cfg.DbType
+	dbName = cfg.DbName
+	user = cfg.Username
+	password = cfg.Password
+	host = cfg.Host
+	port = cfg.Port
 	ds := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", host, port, user, dbName, password)
 	//jdbc:postgresql://10.10.76.215:5432/message?stringtype=unspecified
 	//db, err := gorm.Open("postgres", "host=myhost port=myport user=gorm dbname=gorm password=mypassword")
-	Cloud, err = gorm.Open(dbType, ds)
+	db, err = gorm.Open(dbType, ds)
 
 	if err != nil {
 		logger.LoggerCurrent().WithFields(logrus.Fields{
 			"method": "cloud-connect",
-		}).Panicln("connect to " + config.DbConfig.Host + " failed,err: " + err.Error())
+		}).Panicln("connect to " + cfg.Host + " failed,err: " + err.Error())
 
 		log.Panicln(err)
 	}
 
-	Cloud.SingularTable(true)
-	Cloud.LogMode(true)
-	Cloud.DB().SetMaxIdleConns(10)
-	Cloud.DB().SetMaxOpenConns(100)
+	db.SingularTable(true)
+	db.LogMode(true)
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
 
 	logger.LoggerCurrent().WithFields(logrus.Fields{
 		"method": "db-cloud-connect",
-	}).Infof("connect to " + config.DbConfig.Host + " for " + config.DbConfig.DbName + " is ok")
+	}).Infof("connect to " + cfg.Host + " for " + cfg.DbName + " is ok")
+
+	return db, nil
 }
 
 func CloseCloudDB() {
-	defer Cloud.Close()
+	defer db.Close()
 }
