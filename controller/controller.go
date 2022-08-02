10f208bb0d60764fcd53fc7794bd8c05fc9ea91a
SimonShen <shenzh1990@163.com>
2022-08-02 17:34:25 +0800

    完成controller方法默认生成访问路径，其中开头必须是get post ,默认为get方法。后续可以优化

--- controller/controller.go
+++ controller/controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"reflect"
 	"strings"
@@ -28,29 +27,17 @@ func (c *Controller) initRoute(app *fiber.App, ci ControllerInterface) *fiber.Ap
 			c.ControllerName = conName
 		}
 	}
-	fmt.Println("controllerName:" + c.ControllerName + "  " + r_con_t.String() + " ")
 	// 按照方法名前缀获取action，无法获取默认 get
 	for i := 0; i < typ.NumMethod(); i++ {
-		//fmt.Println(fmt.Sprintf("method[%d]:%s type is %s", i, typ.Method(i).Name, typ.Method(i).Type))
+		method := r_con_v.MethodByName(typ.Method(i).Name)
 		methodName := strings.ToLower(typ.Method(i).Name)
 		urlPath := "/" + c.ControllerName + "/" + methodName
 		if strings.HasPrefix(methodName, "get") {
-			fmt.Println("action get,urlPath:" + strings.ToLower(urlPath))
-			fmt.Println(typ.Method(i).Func.Type().String())
-			method := r_con_v.MethodByName(typ.Method(i).Name)
-			h, ok := (makeHandelFunction(fiber.Handler(nil), method)).(fiber.Handler)
-			//h, ok := (method.Interface()).(fiber.Handler)
-			if !ok {
-				fmt.Println(ok)
-				//panic("error")
-			}
-			app.Add(fiber.MethodGet, strings.ToLower(urlPath), h)
+			app.Add(fiber.MethodGet, strings.ToLower(urlPath), (method.Call(nil)[0].Interface()).(fiber.Handler))
 		} else if strings.HasPrefix(methodName, "post") {
-			fmt.Println("action post,urlPath:" + strings.ToLower(urlPath))
-			//app.Add(fiber.MethodGet, strings.ToLower(urlPath), reflect.New(typ.Elem()))
+			app.Add(fiber.MethodPost, strings.ToLower(urlPath), (method.Call(nil)[0].Interface()).(fiber.Handler))
 		} else {
-			fmt.Println("action default get,urlPath:" + strings.ToLower(urlPath))
-			//app.Add(fiber.MethodGet, strings.ToLower(urlPath), reflect.New(typ.Elem()))
+			app.Add(fiber.MethodGet, strings.ToLower(urlPath), (method.Call(nil)[0].Interface()).(fiber.Handler))
 		}
 
 	}
@@ -61,18 +48,4 @@ func InitController(app *fiber.App, ci ...ControllerInterface) *fiber.App {
 		app = c.initRoute(app, c)
 	}
 	return app
-}
-func makeHandelFunction(f interface{}, vf reflect.Value) interface{} {
-	rf := reflect.TypeOf(f)
-	if rf.Kind() != reflect.Func {
-		panic("expects a function")
-	}
-	//vtf := reflect.ValueOf(f)
-	wrapperF := reflect.MakeFunc(rf, func(in []reflect.Value) []reflect.Value {
-		fmt.Println("before")
-		out := vf.Call(in)
-		fmt.Println("after")
-		return out
-	})
-	return wrapperF.Interface()
 }
