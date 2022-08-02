ee182b6f170ff89b8699a11ad6963dadb2b6651e
SimonShen <shenzh1990@163.com>
2022-08-02 13:56:58 +0800

    新增mainlogo  等配置初始化后修改内容

--- controller/controller.go
+++ controller/controller.go
@@ -30,14 +30,21 @@ func (c *Controller) initRoute(app *fiber.App, ci ControllerInterface) *fiber.Ap
 	}
 	fmt.Println("controllerName:" + c.ControllerName + "  " + r_con_t.String() + " ")
 	// 按照方法名前缀获取action，无法获取默认 get
-	for i := 0; i < r_con_v.NumMethod(); i++ {
+	for i := 0; i < typ.NumMethod(); i++ {
 		//fmt.Println(fmt.Sprintf("method[%d]:%s type is %s", i, typ.Method(i).Name, typ.Method(i).Type))
 		methodName := strings.ToLower(typ.Method(i).Name)
 		urlPath := "/" + c.ControllerName + "/" + methodName
 		if strings.HasPrefix(methodName, "get") {
 			fmt.Println("action get,urlPath:" + strings.ToLower(urlPath))
-
-			//app.Add(fiber.MethodGet, strings.ToLower(urlPath), fiber.Handler())
+			fmt.Println(typ.Method(i).Func.Type().String())
+			method := r_con_v.MethodByName(typ.Method(i).Name)
+			h, ok := (makeHandelFunction(fiber.Handler(nil), method)).(fiber.Handler)
+			//h, ok := (method.Interface()).(fiber.Handler)
+			if !ok {
+				fmt.Println(ok)
+				//panic("error")
+			}
+			app.Add(fiber.MethodGet, strings.ToLower(urlPath), h)
 		} else if strings.HasPrefix(methodName, "post") {
 			fmt.Println("action post,urlPath:" + strings.ToLower(urlPath))
 			//app.Add(fiber.MethodGet, strings.ToLower(urlPath), reflect.New(typ.Elem()))
@@ -54,4 +61,18 @@ func InitController(app *fiber.App, ci ...ControllerInterface) *fiber.App {
 		app = c.initRoute(app, c)
 	}
 	return app
+}
+func makeHandelFunction(f interface{}, vf reflect.Value) interface{} {
+	rf := reflect.TypeOf(f)
+	if rf.Kind() != reflect.Func {
+		panic("expects a function")
+	}
+	//vtf := reflect.ValueOf(f)
+	wrapperF := reflect.MakeFunc(rf, func(in []reflect.Value) []reflect.Value {
+		fmt.Println("before")
+		out := vf.Call(in)
+		fmt.Println("after")
+		return out
+	})
+	return wrapperF.Interface()
 }
