b8b287cb4bc5d8e693732e76cb423594e4bd92db
SherrillJoyceGit <43162021+SherrillJoyceGit@users.noreply.github.com>
2022-08-12 10:25:40 +0800

    fix(package db): 修复代码合并后的一些小问题

--- main.go
+++ main.go
@@ -3,12 +3,6 @@ package main
 import (
 	"github.com/SherrillJoyceGit/go-bass-scaffold/api"
 	"github.com/SherrillJoyceGit/go-bass-scaffold/bootstrap"
-	"github.com/SherrillJoyceGit/go-bass-scaffold/config"
-	"github.com/SherrillJoyceGit/go-bass-scaffold/controller"
-	"github.com/SherrillJoyceGit/go-bass-scaffold/controller/dao"
-	"github.com/SherrillJoyceGit/go-bass-scaffold/db"
-	"go.uber.org/dig"
-	"log"
 )
 
 func main() {
